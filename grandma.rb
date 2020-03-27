e2183515947d1ebfa48c3653b5fc98b75af8d50e
Saida Davis <saihdavis@gmail.com>
2020-03-27 23:39:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
    elsif phrase == "Hi Nana, how are you?"  
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "Hi"
+  elsif phrase == "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
