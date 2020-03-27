affc4ce4d8c473e33c302708901cec0c5695e3a9
Saida Davis <saihdavis@gmail.com>
2020-03-27 23:04:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@ def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+
+ elsif phrase == "Hi Nana, how are you?"
+    puts "HUH?! SPEAK UP, SONNY".upcase
  
 end
-
-
-
 
 
 
