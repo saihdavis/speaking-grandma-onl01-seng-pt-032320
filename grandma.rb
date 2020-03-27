edf1e3abda19a5bf5d3951e8516eb0b32cf62392
Saida Davis <saihdavis@gmail.com>
2020-03-27 23:15:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,18 +1,9 @@
 def speak_to_grandma(phrase)
   
-  if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
  elsif phrase == "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY".upcase
-  elsif phrase == "Hi"
-    puts "HUH?! SPEAK UP, SONNY".upcase
-  elsif phrase == "WHAT DID YOU EAT TODAY?"
-    puts "NO, NOT SINCE 1938!".upcase
-  elsif phrase == "WHAT?".upcase
-    puts "NO, NOT SINCE 1938!".upcase
-    
+  
   end
-end
 
 
 
